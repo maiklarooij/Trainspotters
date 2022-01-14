c76371122091e24cff70fba47bdb0d4babb3f592
maiklarooij <mklarooij@gmail.com>
2022-01-14 11:06:27 +0100

    Update routemap.py with some comments

--- routemap.py
+++ routemap.py
@@ -1,3 +1,12 @@
+# -----------------------------------------------------------
+# routemap.py
+#
+# contains class definitions of objects in the RailNL case:
+# connections, routes and routemap
+#
+# authors: Mijntje Meijer, Sam Bijhouwer and Maik Larooij
+# -----------------------------------------------------------
+
 import csv
 import random
 
@@ -6,6 +15,7 @@ def load_data(scale = 'Holland'):
     Load in the necessary .csv files. Options: 
     scale = 'Holland' or 'Nationaal'
     """
+
     with open(f"data/Connecties{scale}.csv", mode="r") as file:
         connections_csv = list(csv.reader(file))
     
@@ -14,69 +24,109 @@ def load_data(scale = 'Holland'):
 
     return {'connections': connections_csv, 'stations': stations_csv}
 
-# Define max route time and total stations
+# Define max route time, scale and total stations
 MAX_ROUTE_TIME = 120
 SCALE = 'Holland'
 TOTAL_STATIONS = len(load_data(scale = SCALE)['stations'][1:])
 
 class Connection:
-    """ Object which represents a connection """
+    """ 
+    Object which represents a connection
+    Has an origin station, a destination station and a distance between the stations
+    """
+
     def __init__(self, origin, destination, distance):
+
         self.origin = origin
         self.destination = destination
         self.distance = distance
 
     def __str__(self):
+        """ Writes some information about the connection """
+
         return f"{self.origin} to {self.destination}: {self.distance}"
 
 
 class Route:
-    """ Object which represents multiple connections, forming a route """
+    """ 
+    Object which represents multiple connections, forming a route 
+    A route consists of a list of connections and a set of connected stations
+    """
+
     def __init__(self):
+
         self.connections = []
         self.stations = set()
 
     def add_connection(self, connection):
-        """Adds a connection to the route if the total distance does not exceed 120 minutes"""
+        """ Adds a connection to the route if the total distance does not exceed 120 minutes """
+
         if sum(c.distance for c in self.connections) + connection.distance > MAX_ROUTE_TIME:
             return False
         else:
             self.connections.append(connection)
+
+            # Also add origin and destination station of the connection to set of stations
             self.stations.add(connection.origin)
             self.stations.add(connection.destination)
+
             return True
 
     def __str__(self):
+        """ Writes some information about the connection """
+
         return ", ".join(str(connection) for connection in self.connections)
 
 
 class Routemap:
-    """ Object which represents all routes, forming a routemap """
+    """ 
+    Object which represents all routes, forming a routemap 
+    A routemap consists of Route objects
+    """
     def __init__(self, amount):
         self.routes = {i:Route() for i in range(amount)}
-        self.stations = set()
     
     def print_solution(self):
+        """ 
+        Writes some information about the routemap
+        Printing the connections, total route time and final score
+        """
+
         for i, route in self.routes.items():
+
             print(f"Route #{i}: ")
             t_time = 0
+
             for connection in route.connections:
                 t_time += connection.distance
                 print(connection)
+
             print(f"Total Route time: {t_time}\n")
         
         print(f"Score of routemap: {self.calc_score()}")
 
     def calc_score(self):
-        Min = 0
+        """ 
+        Calculates the score of the routemap according to the formula:
+        K = P*10000 - (T*100 + M)
+        
+        P = Fractions of all stations included in routemap
+        T = Number of routes used in routemap
+        M = Total time of all routes in minutes
+        """
+
+        # Calculate total time of all routes
+        M = 0
         for route in self.routes.values():
             for connection in route.connections:
-                Min += connection.distance
+                M += connection.distance
         
-        # Calculate number of stations - TODO: maybe make this easier?
-        frac_stations = len({item for sublist in [list(route.stations) for route in self.routes.values()] for item in sublist}) / TOTAL_STATIONS
-        print({item for sublist in [list(route.stations) for route in self.routes.values()] for item in sublist})
-        score = (frac_stations * 10000) - (len(self.routes) * 100 + Min)
+        # Calculate number of stations and number of routes - TODO: maybe make this easier?
+        P = len({item for sublist in [list(route.stations) for route in self.routes.values()] for item in sublist}) / TOTAL_STATIONS
+        T = len(self.routes)
+
+        # Calculate final score
+        score = (P * 10000) - (T * 100 + M)
 
         return score
 
@@ -99,12 +149,10 @@ class Routemap:
             writer.writerow(footer)
 
     def fill_routemap_random(self, connections_data):
-        """ 
-        Fills the routemap on a random basis
-        """
+        """ Fills the routemap on a random basis """
 
         for line in connections_data[1:]:
-            c = Connection(line[0], line[1], int(line[2]))
+            c = Connection(line[0], line[1], float(line[2]))
             while True:
                 random_route = random.randint(0, 6)
                 if self.routes[random_route].add_connection(c):
